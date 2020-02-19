db05e401ea0fa64f5943795e672d1bd7c520c8ac
EricAndreR <ericandre89@gmail.com>
2020-02-19 01:28:32 +0100

    Created link counter.

--- ProblemOne.py
+++ ProblemOne.py
@@ -1,9 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
 
-response = requests.get('https://www.meltgroup.com/en/')
+url = 'https://www.meltgroup.com/en/'
+
+response = requests.get(url)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 
+counter = 0
+
 for links in soup.find_all('a', href=True):
     print(links['href'])
+    counter = counter + 1
+
+print("En la pagina " + url + "hay un total de " + str(counter) + " links encontrados.")
